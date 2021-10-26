0e0e3baa92893e7494c8ab3f91fc0a7b358c32be
Uchchash346 <mru416@gmail.com>
2021-10-26 21:49:22 +0600

    airbnb-clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import Home from './Home';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import SearchPage from './SearchPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </ Router>
+
+      {/* Home */}
+      {/* Header */}
+      {/* Banner */}
+      {/* Cards */}
+      {/* Footer */}
+      {/* Search Page */}
+
     </div>
   );
 }
 
 export default App;
+
+// 
